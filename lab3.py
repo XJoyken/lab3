ab45b4448ee61e010a0839964c887cbc77c3dc8e
Abdurakhmanov_Daniyel <daniel.kapchagay@gmail.com>
2025-02-02 18:25:03 +0500

    Python Lambda

--- lab3.py
+++ lab3.py
@@ -145,3 +145,51 @@ def tri_recursion(k):
 
 print("Recursion Example Results:")
 tri_recursion(6)
+
+
+#Python Lambda:
+#A lambda function is a small anonymous function.
+#A lambda function can take any number of arguments, but can only have one expression.
+
+#Syntax
+#lambda arguments : expression
+
+#Add 10 to argument a, and return the result:
+
+x = lambda a : a + 10
+print(x(5))
+
+#Lambda functions can take any number of arguments:
+x = lambda a, b : a * b
+print(x(5, 6))
+
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+#Why Use Lambda Functions?
+#The power of lambda is better shown when you use them as an anonymous function inside another function.
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+print(mydoubler(11))
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(3)
+print(mydoubler(11))
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+print(mydoubler(11))
+print(mytripler(11))
+
+#Use lambda functions when an anonymous function is required for a short period of time.
